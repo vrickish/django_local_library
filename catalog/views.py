5706d07017520a2367d6178550bfd25bef3e1608
vrickcode <vrickcode@gmail.com>
2025-08-20 09:30:47 +0100

    Created the homepage and added the extra challenge exercise

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    # Count for the genres
+    num_genres = Genre.objects.count()
+
+
+    # Count for Books that contain the word 'the'
+    num_books_with_the = Book.objects.filter(title__icontains = 'the').count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_with_the': num_books_with_the,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
